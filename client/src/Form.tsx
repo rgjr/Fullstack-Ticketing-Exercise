34d2a71db2c155fb45a9b7be04111b5b529c9e80
Raul Gutierrez <rdgutierrez@gmail.com>
2025-05-13 11:44:54 -0700

    add readme, fix null data

--- client/src/Form.tsx
+++ client/src/Form.tsx
@@ -13,7 +13,7 @@ interface Ticket {
 
 const Form = () => {
   const { id } = useParams();
-  const [data, setData] = useState<Ticket[]>([]);
+  const [data, setData] = useState<Ticket | null>(null);
   const [status, setStatus] = useState('');
   const [notes, setNotes] = useState('');
 
@@ -29,8 +29,8 @@ const Form = () => {
   const saveData = async () => {
     try {
       const response = await axios.patch(`http://localhost:3000/tickets/${id}`, {
-        status: status ? status : data.status,
-        notes: notes ? notes : data.notes
+        status: status ? status : data?.status,
+        notes: notes ? notes : data?.notes
       });
 
       return response
@@ -55,23 +55,23 @@ const Form = () => {
     <form className='max-w-sm mx-auto'>
       <div className='mb-5'>
         <label htmlFor="fname" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>ID</label>
-        <input disabled placeholder={`${data.id}`} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        <input disabled placeholder={`${data?.id || ''}`} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
       </div>
       <div className='mb-5'>
         <label htmlFor="fname" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Customer Name</label>
-        <input disabled placeholder={`${data.customerName}`} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        <input disabled placeholder={`${data?.customerName || ''}`} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
       </div>
       <div className='mb-5'>
         <label htmlFor="fname" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Email</label>
-        <input disabled placeholder={`${data.email}`} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        <input disabled placeholder={`${data?.email || ''}`} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
       </div>
       <div className='mb-5'>
         <label htmlFor="fname" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Status</label>
-        <input placeholder={`${data.status}`} onChange={handleStatusChange} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        <input placeholder={`${data?.status || ''}`} onChange={handleStatusChange} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
       </div>
       <div className='mb-5'>
         <label htmlFor="fname" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Notes</label>
-        <input placeholder={`${data.notes}`} onChange={handleNotesChange} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        <input placeholder={`${data?.notes || ''}`} onChange={handleNotesChange} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
       </div>
       <div onClick={saveData} type='submit' className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
         Save
